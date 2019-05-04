ced8588af201c9acb432762a25c88f15efaf1f2d
uzochukwueddie <uzo.odozi@gmail.com>
2019-05-04 16:06:57 +0200

    Updated package json file

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,23 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+import { Flashlight } from '@ionic-native/flashlight/ngx';
 
 import { HomePage } from './home.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePage
+  }
+];
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild([
-      {
-        path: '',
-        component: HomePage
-      }
-    ])
+    RouterModule.forChild(routes)
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  providers: [Flashlight]
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { Flashlight } from '@ionic-native/flashlight/ngx';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
 export class HomePage {
+  isLightOn = false;
+
+  constructor(
+    private flashLight: Flashlight,
+    private platform: Platform,
+    private alertController: AlertController
+  ) { }
+
+  lightOn() {
+    this.isLightOn = !this.isLightOn;
+    this.platform.ready().then(() => {
+      if (this.flashLight.available()) {
+        this.flashLight.switchOn();
+      } else {
+        this.showAlert();
+      }
+    });
+  }
+
+  lightOff() {
+    this.isLightOn = !this.isLightOn;
+    this.flashLight.switchOff();
+  }
+
+  async showAlert() {
+    const alert = await this.alertController.create({
+      header: 'Error',
+      message: 'Flash light is not available',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 
 }
